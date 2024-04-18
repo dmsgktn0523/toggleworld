eb8ede16b25a20b31e0a33cb27b06e7086aaf03c
dmsgktn0523 <dmsgktn0523@naver.com>
2024-04-18 23:59:23 +0900

    Day 10 after updating - Created new projects from words2 - added fragments - embedded dictionary page

---
+++ app/src/main/java/com/hello/toggleworld/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.hello.toggleworld
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.hello.toggleworld.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnFragment1.setOnClickListener {
+            setFrag(0)
+        }
+        binding.btnFragment2.setOnClickListener {
+            setFrag(1)
+        }
+
+//        setFrag(0)
+    }
+
+    private fun setFrag(fragNum : Int) {
+        val ft = supportFragmentManager.beginTransaction()
+        when(fragNum)
+        {
+            0 -> {
+                ft.replace(R.id.main_frame,Fragment1()).commit()
+            }
+            1 -> {
+                ft.replace(R.id.main_frame,Fragment2()).commit()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.main_frame)
+        if (fragment is Fragment2) {
+            fragment.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
